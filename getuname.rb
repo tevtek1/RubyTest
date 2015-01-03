69d5eede965fbf34b3eb25ec9f18ac42d0458911
System Administrator <root@tevtek3.home>
2015-01-02 23:13:33 -0500

    Create platform test

--- getuname.rb
+++ getuname.rb
@@ -1,4 +1,15 @@
-system_uname = `uname -a`
+#Get platform type
+system_uname = `uname -s`
 
 puts system_uname
+puts system_uname.length
+
+ll = system_uname[0..5]
+puts ll
+
+if ll == "Darwin"
+	puts "True"
+else
+	puts "false"
+end
 
